0cc94a62579991e64586de4459cfb01bb79056d5
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-05-18 16:30:17 +0200

    error prevention for fetching

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import React from "react";
 export default function Home() {
   const { pizzas, loading, error } = useFetchPizzas();
   console.log(pizzas);
+
+  if (pizzas === null || pizzas === undefined) {
+    return <div>{loading}</div>;
+  }
   return (
     <main>
       {Object.keys(pizzas).map((i) => {
