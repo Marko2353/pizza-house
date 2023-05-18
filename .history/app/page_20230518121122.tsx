7bbc3cb9a7b94f77d245ca9981871305aaf33c49
ZuRi-To <91980222+Inigo-Zuriaga@users.noreply.github.com>
2023-05-18 12:11:33 +0200

    hello
    hi

--- .history/app/page_20230518121122.tsx
+++ .history/app/page_20230518121122.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import React from "react";
 
-export default function Home() { 
+export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
