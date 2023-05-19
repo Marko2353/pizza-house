76a8ca5745e1d43a94a2e0800fc333db7e0e6ffe
ZuRi-To <91980222+Inigo-Zuriaga@users.noreply.github.com>
2023-05-19 15:12:07 +0200

    sd

---
+++ .history/components/buttons/CallToAction_20230519145929.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+import "../../app/globals.css";
+
+export default function CallToAction({ name, link }) {
+  return (
+    <Link href={link} className="call-to-action box-shadow">
+      {name}
+    </Link>
+  );
+}
