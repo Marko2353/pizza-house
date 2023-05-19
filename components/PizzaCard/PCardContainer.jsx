a2d029b62089b28b3fb445f34aaeb2af7b0272d2
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-05-19 17:30:39 +0200

    card

--- components/PizzaCard/PCardContainer.jsx
+++ components/PizzaCard/PCardContainer.jsx
@@ -16,23 +16,25 @@ export default function PCardContainer() {
     return <div>{loading}</div>;
   }
   return (
-    <>
-      {Object.keys(pizzas).map((pizzaId) => {
-        const pizzaName = pizzas[pizzaId];
-        const pizzaDescription = descriptions[pizzaId];
+    <div className="container flex flex-wrap gap-4 m-auto">
+      <>
+        {Object.keys(pizzas).map((pizzaId) => {
+          const pizzaName = pizzas[pizzaId];
+          const pizzaDescription = descriptions[pizzaId];
 
-        return (
-          <div className="flex container">
-            <div className="bg-light inline-block shadow-xl">
-              <h2 className="text-3xl mx-auto mb-8 my-2 text-center">
+          return (
+            <div className="flex flex-col bg-light shadow-xl">
+              <h2 className="text-3xl mx-auto mb-8 my-2 text-center w-[10ch]">
                 {pizzaName}
               </h2>
               <Image className="mx-auto mb-8" src={PizzaImg} alt="" />
-              <p className="mb-4 mx-8">{pizzaDescription}</p>
+              <p className="mb-4 mx-8 w-[20ch] text-center">
+                {pizzaDescription}
+              </p>
             </div>
-          </div>
-        );
-      })}
-    </>
+          );
+        })}
+      </>
+    </div>
   );
 }
