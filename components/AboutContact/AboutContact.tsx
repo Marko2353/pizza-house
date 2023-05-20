d02c33e0aa85020cd53db8cd4e95e2660710f6e9
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-05-20 14:35:09 +0200

    s

--- components/AboutContact/AboutContact.tsx
+++ components/AboutContact/AboutContact.tsx
@@ -3,7 +3,7 @@ import Button from "../buttons/Button";
 
 export default function AboutContact({ title, description, button }) {
   return (
-    <section className="container flex flex-col items-center m-auto my-20">
+    <section className="container flex flex-col items-center m-auto my-10">
       <h2>{title}</h2>
       <p className="w-[60ch] text-center my-7">{description}</p>
       <Button name={button} link={"#"} />
