c00185604e08fb34d97280bd39ef3ca179aefef5
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-05-25 16:17:06 +0200

    s

--- components/PizzaCard/PCardCarousel.jsx
+++ components/PizzaCard/PCardCarousel.jsx
@@ -1,79 +1,79 @@
-"use client";
-import React, { useRef } from "react";
-import PizzaCard from "./PizzaCard";
-import useFetchPizzas from "../../hooks/fetchPizzas";
-import Image from "next/image";
-import Arrow from "../../public/img/arrow.svg";
+// "use client";
+// import React, { useRef } from "react";
+// import PizzaCard from "./PizzaCard";
+// import useFetchPizzas from "../../hooks/fetchPizzas";
+// import Image from "next/image";
+// import Arrow from "../../public/img/arrow.svg";
 
-export default function PCardCarousel() {
-  const { pizzas, descriptions, loading, error } = useFetchPizzas();
-  const carouselRef = useRef(null);
+// export default function PCardCarousel() {
+//   const { pizzas, descriptions, loading, error } = useFetchPizzas();
+//   const carouselRef = useRef(null);
 
-  if (
-    pizzas === null ||
-    pizzas === undefined ||
-    descriptions === null ||
-    descriptions === undefined
-  ) {
-    return <div>{loading}</div>;
-  }
+//   if (
+//     pizzas === null ||
+//     pizzas === undefined ||
+//     descriptions === null ||
+//     descriptions === undefined
+//   ) {
+//     return <div>{loading}</div>;
+//   }
 
-  const scrollToNext = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({
-        left: carouselRef.current.offsetWidth,
-        behavior: "smooth",
-      });
-    }
-  };
+//   const scrollToNext = () => {
+//     if (carouselRef.current) {
+//       carouselRef.current.scrollBy({
+//         left: carouselRef.current.offsetWidth,
+//         behavior: "smooth",
+//       });
+//     }
+//   };
 
-  const scrollToPrevious = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({
-        left: -carouselRef.current.offsetWidth,
-        behavior: "smooth",
-      });
-    }
-  };
+//   const scrollToPrevious = () => {
+//     if (carouselRef.current) {
+//       carouselRef.current.scrollBy({
+//         left: -carouselRef.current.offsetWidth,
+//         behavior: "smooth",
+//       });
+//     }
+//   };
 
-  const pizzaCards = Object.keys(pizzas).map((pizzaId) => {
-    const pizzaName = pizzas[pizzaId];
-    const pizzaDescription = descriptions[pizzaId];
-    return (
-      <div className="flex mr-4 " key={pizzaId}>
-        <PizzaCard pizzaName={pizzaName} pizzaDescription={pizzaDescription} />
-      </div>
-    );
-  });
+//   const pizzaCards = Object.keys(pizzas).map((pizzaId) => {
+//     const pizzaName = pizzas[pizzaId];
+//     const pizzaDescription = descriptions[pizzaId];
+//     return (
+//       <div className="flex mr-4 " key={pizzaId}>
+//         <PizzaCard pizzaName={pizzaName} pizzaDescription={pizzaDescription} />
+//       </div>
+//     );
+//   });
 
-  return (
-    <section className="relative py-10 m-auto bg-dark">
-      <h1 className="text-center">Our pizzas</h1>
-      <p className="w-[60ch] mx-auto my-5 text-center text-light">
-        Lorem ipsum dolor sit amet consectetur. Consectetur donec maecenas nisl
-        pellentesque amet non. Pellentesque aliquam
-      </p>
-      <div className="absolute ml-56 transform -translate-y-1/2 left-4 top-1/2">
-        <button
-          className="p-2 rounded-full bg-dark hover:bg-gray-400 focus:outline-none"
-          onClick={scrollToPrevious}
-        >
-          <Image src={Arrow} alt="" />
-        </button>
-      </div>
-      <div className="container relative w-2/4 mx-auto">
-        <div className="flex overflow-x-hidden" ref={carouselRef}>
-          <div className="flex">{pizzaCards}</div>
-        </div>
-      </div>
-      <div className="absolute mr-56 transform -translate-y-1/2 right-4 top-1/2">
-        <button
-          className="p-2 rounded-full bg-dark hover:bg-gray-400 focus:outline-none"
-          onClick={scrollToNext}
-        >
-          <Image src={Arrow} alt="" className="transform rotate-180" />
-        </button>
-      </div>
-    </section>
-  );
-}
+//   return (
+//     <section className="relative py-10 m-auto bg-dark">
+//       <h1 className="text-center">Our pizzas</h1>
+//       <p className="w-[60ch] mx-auto my-5 text-center text-light">
+//         Lorem ipsum dolor sit amet consectetur. Consectetur donec maecenas nisl
+//         pellentesque amet non. Pellentesque aliquam
+//       </p>
+//       <div className="absolute ml-56 transform -translate-y-1/2 left-4 top-1/2">
+//         <button
+//           className="p-2 rounded-full bg-dark hover:bg-gray-400 focus:outline-none"
+//           onClick={scrollToPrevious}
+//         >
+//           <Image src={Arrow} alt="" />
+//         </button>
+//       </div>
+//       <div className="container relative w-2/4 mx-auto">
+//         <div className="flex overflow-x-hidden" ref={carouselRef}>
+//           <div className="flex">{pizzaCards}</div>
+//         </div>
+//       </div>
+//       <div className="absolute mr-56 transform -translate-y-1/2 right-4 top-1/2">
+//         <button
+//           className="p-2 rounded-full bg-dark hover:bg-gray-400 focus:outline-none"
+//           onClick={scrollToNext}
+//         >
+//           <Image src={Arrow} alt="" className="transform rotate-180" />
+//         </button>
+//       </div>
+//     </section>
+//   );
+// }
--- components/PizzaCard/PCardContainer.jsx
+++ components/PizzaCard/PCardContainer.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import PizzaCard from "./PizzaCard";
-import useFetchPizzas from "../../hooks/fetchPizzas";
+// import React from "react";
+// import PizzaCard from "./PizzaCard";
+// import useFetchPizzas from "../../hooks/fetchPizzas";
 
-export default function PCardContainer() {
-  const { pizzas, descriptions, loading, error } = useFetchPizzas();
+// export default function PCardContainer() {
+//   const { pizzas, descriptions, loading, error } = useFetchPizzas();
 
-  if (
-    pizzas === null ||
-    pizzas === undefined ||
-    descriptions === null ||
-    descriptions === undefined
-  ) {
-    return <div>{loading}</div>;
-  }
-  return (
-    <div className="container flex flex-wrap gap-4 m-auto">
-      <>
-        {Object.keys(pizzas).map((pizzaId, key) => {
-          const pizzaName = pizzas[pizzaId];
-          const pizzaDescription = descriptions[pizzaId];
+//   if (
+//     pizzas === null ||
+//     pizzas === undefined ||
+//     descriptions === null ||
+//     descriptions === undefined
+//   ) {
+//     return <div>{loading}</div>;
+//   }
+//   return (
+//     <div className="container flex flex-wrap gap-4 m-auto">
+//       <>
+//         {Object.keys(pizzas).map((pizzaId, key) => {
+//           const pizzaName = pizzas[pizzaId];
+//           const pizzaDescription = descriptions[pizzaId];
 
-          return (
-            <PizzaCard
-              key={key}
-              pizzaName={pizzaName}
-              pizzaDescription={pizzaDescription}
-            />
-          );
-        })}
-      </>
-    </div>
-  );
-}
+//           return (
+//             <PizzaCard
+//               key={key}
+//               pizzaName={pizzaName}
+//               pizzaDescription={pizzaDescription}
+//             />
+//           );
+//         })}
+//       </>
+//     </div>
+//   );
+// }
