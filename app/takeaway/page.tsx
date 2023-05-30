ec8ab4249f4451c30b93f4e6347f89d409eab37b
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-05-30 14:02:27 +0200

    s

--- app/takeaway/page.tsx
+++ app/takeaway/page.tsx
@@ -25,21 +25,6 @@ export default async function Takeaway() {
             {pizzaProducts.map((product) => (
               <PizzaProduct key={product.id} product={product} />
             ))}
-            {/* {Object.keys(pizzas?.title).map((pizzaId) => {
-      const pizzaName = pizzas?.title[pizzaId];
-      const pizzaDescription = pizzas?.description[pizzaId];
-      const pizzaPrice = pizzas?.price[pizzaId];
-
-      return (
-        <PizzaCard
-          key={pizzaId}
-          pizzaName={pizzaName}
-          pizzaDescription={pizzaDescription}
-          price={pizzaPrice}
-          click={undefined}
-        />
-      );
-    })} */}
           </div>
 
           <CheckoutBox />
